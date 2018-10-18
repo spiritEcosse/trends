5dc31c724a2eeeab2070eeb237f6c65a328a41c2
igor <shevchenkcoigor@gmail.com>
2018-10-18 12:25:19 +0300

    print res

--- trends/tasks.py
+++ trends/tasks.py
@@ -7,4 +7,5 @@ from pytrends.request import TrendReq
 def bit_google_trends():
     pytrends = TrendReq(hl='en-US', tz=360)
     kw_list = ["Blockchain"]
-    pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
+    res = pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
+    print(res)
