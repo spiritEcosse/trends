73831ff406d4fee53cf8ffd9c819ea803dda94fa
igor <shevchenkcoigor@gmail.com>
2018-10-23 18:00:21 +0300

    add selenium

--- trends/tasks.py
+++ trends/tasks.py
@@ -2,14 +2,17 @@ from __future__ import absolute_import
 
 import itertools
 
-from .celery import app
-from .settings import TOKEN_SHUTTERSTOCK
+from trends.celery import app
+from trends.settings import TOKEN_SHUTTERSTOCK
 
 from pytrends.request import TrendReq
 
 from shutterstock.api import ShutterstockAPI
 
 from shutterstock_api.resources import Image
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 @app.task
@@ -23,9 +26,17 @@ def shutterstock_search():
     Image.API = ShutterstockAPI(token=TOKEN_SHUTTERSTOCK)
     images = Image.list(view='full')
 
+    driver = webdriver.Remote(
+        command_executor='http://firefox:4444/wd/hub',
+        desired_capabilities={'browserName': 'firefox'},
+    )
+    driver.get("http://research.picworkflow.com")
+    elem = driver.find_element_by_id("search")
+
     for image in images:
         stuff = sorted(image.keywords)
 
         for L in range(0, len(stuff) + 1):
             for subset in itertools.combinations(stuff, L):
-                print(subset)
+                elem.send_keys(' '.join(subset))
+                element.submit()
